605aae71662f67fa6d3a4e34d09c45a3e34ef7fc
kkuuk <kkuuk@gmail.com>
2021-10-22 16:52:13 +0900

    db 연동 완료.

--- crawlling/dbconfig.py
+++ crawlling/dbconfig.py
@@ -1,3 +1,23 @@
 import MySQLdb
 
-conn = MySQLdb.connect(user="root", passwd="root", host="localhost", db="crawlling")
+conn = MySQLdb.connect(user="root", passwd="root", host="localhost", port=3306, db="crawling", charset='utf8')
+
+
+def insert_sql(data_list):
+    try:
+        with conn.cursor() as curs:
+            print("db 연결")
+            # create_nm = "현대1"
+            # carmd_nm = "아반떼1"
+            # sql = 'insert into dbtest(create_nm, carmd_nm) values(%s, %s)'  # db 컬럼명을 생략하려면 values 에 컬럼만큼 값을 설정해 줘야되고 아니라면 명시를 해줘야함.
+            # curs.execute(sql, [create_nm, carmd_nm])
+            sql = 'insert into crawling_data1 values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, ' \
+                  '%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+            curs.execute(sql, data_list)
+            conn.commit()
+    except:
+        print("db 에러")
+        print(curs.execute(sql))
+
+    finally:
+        conn.close()
--- crawlling/main.py
+++ crawlling/main.py
@@ -3,6 +3,7 @@ import time
 
 from selenium import webdriver
 import chromedriver_autoinstaller
+import dbconfig
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -10,10 +11,10 @@ chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
 
 # 드라이버 설치
 try:
-    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver')  # 해당경로에 드라이버가 있으면 불러오고
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')  # 해당경로에 드라이버가 있으면 불러오고
 except:
     chromedriver_autoinstaller.install(True)
-    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver')  # 없으면 설치
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')  # 없으면 설치
     driver.close()
 
 url = 'https://app.modeal.net'
@@ -53,7 +54,6 @@ for e in maker:
 
                 for l in car_line:
                     if l == '아반떼':
-                        # print("아반떼")
                         a = driver.find_element_by_xpath('//*[@id="wrap"]/section/div/ul/li[1]/ul/li[1]/a')
                         a.click()
                         time.sleep(3)
@@ -61,7 +61,6 @@ for e in maker:
                         fuel = fuel.text.split('\n')
                         # print(fuel)
                         for f in fuel:
-                            # print(f)
                             if f == '가솔린 1.6  [2020년형]':
                                 a = driver.find_element_by_xpath('//*[@id="wrap"]/section/div/ul/li[1]/a')
                                 a.click()
@@ -82,19 +81,18 @@ for e in maker:
                                         time.sleep(3)
                                         month = driver.find_element_by_xpath(
                                             '/html/body/div[2]/section/article[4]/form/div/div[1]/div/div/select')
-                                        print(month.text)
+                                        # print(month.text)
 
                                         month = month.text.split('\n')
-                                        print(month)
+                                        # print(month)
 
                                         if month[2]:
                                             print(month[2])
                                             month_36 = driver.find_element_by_xpath('//*[@id="PERD_CD"]/option[3]')
                                             month_36.click()
                                             time.sleep(3)
-                                            adpay = driver.find_elements_by_css_selector('#PAYPRO_text')
-                                            # print(adpay[0].text)
                                             adpay = driver.find_element_by_xpath('//*[@id="PAYPRO_text"]/option[1]')
+                                            # print(adpay.text)
                                             adpay.click()
                                             time.sleep(3)
                                             # print(adpay.text)
@@ -102,26 +100,36 @@ for e in maker:
                                             # adpay = adpay.text.split('\n')
                                             # print(adpay)  # 선수금
                                             if adpay.text.__contains__("0%"):
-                                                print("타냐?")
+                                                print("36개월")
+                                                adpay = adpay.text.split("|")
+                                                print(adpay)
                                                 deposit = driver.find_element_by_xpath('//*[@id="DEPPRO_text"]')
                                                 # print(deposit.text) # 보증금
+                                                deposit_text = deposit.text
                                                 deposit = deposit.text.split('\n')
                                                 # print(deposit[0])
+                                                deposit = deposit[0].split("|")
+                                                print(deposit)
                                                 residual = driver.find_element_by_xpath(
                                                     '//*[@id="REPRT_AMT"]/option[2]')  # 최대잔가 고정 리스트 출력이 안됨.
                                                 # print(residual.text)  # 잔존가치
                                                 dits = driver.find_element_by_xpath(
-                                                    '//*[@id="STIP_DST_CD"]/option[1]')  # 2만키로 고정 리스트 출력이 안됨.
+                                                    '//*[@id="STIP_DST_CD"]/option[3]')  # 2만키로 고정 디폴트 2만 돼있어서 클릭이벤트를 주진 않았음.
                                                 # print(dits.text)
-                                                city = driver.find_element_by_xpath('//*[@id="PBDBT_CITY_CD"]')
+                                                # city = driver.find_element_by_xpath('//*[@id="PBDBT_CITY_CD"]') # 리스트로 출력됨
+                                                # print(city.text, "리스트?")
+                                                city = driver.find_element_by_xpath('//*[@id="PBDBT_CITY_CD"]/option[11]')
                                                 # print(city.text)
+
                                                 merits = driver.find_element_by_xpath(
                                                     '//*[@id="e2_list"]/tr[1]/td[1]/label')
-                                                print(merits.text)
+                                                # print(merits.text)
                                                 monthly_pay = driver.find_element_by_xpath(
                                                     '//*[@id="e2_list"]/tr[1]/td[2]')
-                                                print(monthly_pay.text)
-
+                                                # print(monthly_pay.text)
+                                                # print(e, n, l, f, m, month[2], adpay[1],  deposit[1], residual.text, dits.text, city.text, merits.text, monthly_pay.text)
+                                                data_list = [e, n, l, f, m, month[2], adpay[1],  deposit[1], residual.text, dits.text, city.text, merits.text, monthly_pay.text, " ", " ", " ", " ", " ", " ", " ", " "]
+                                                dbconfig.insert_sql(data_list)
 
                                         if month[3]:
                                             print(month[3])
@@ -138,7 +146,7 @@ for e in maker:
                                             # adpay = adpay.text.split('\n')
                                             # print(adpay)  # 선수금
                                             if adpay.text.__contains__("0%"):
-                                                print("타냐?")
+                                                # print("타냐?")
                                                 deposit = driver.find_element_by_xpath('//*[@id="DEPPRO_text"]')
                                                 # print(deposit.text) # 보증금
                                                 deposit = deposit.text.split('\n')
